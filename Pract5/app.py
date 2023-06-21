25f21c24ed66bb66c828621c8f5a6e157d37dc79
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2023-06-21 17:06:46 -0600

    Practica6 Final
    Version final de la practica 6

--- Pract5/app.py
+++ Pract5/app.py
@@ -1,5 +1,5 @@
 #importacion del framework
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for,flash
 from flask_mysqldb import MySQL
 
 #Inicializacion del APP
@@ -8,6 +8,7 @@ app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='dbflask'
+app.secret_key= 'mysecrety'
 mysql= MySQL(app)
 
 #Declaracion de ruta http://localhost:5000
@@ -20,12 +21,19 @@ def index():
 @app.route('/guardar',methods=['POST'])
 def guardar():
   if request.method == 'POST':
-    titulo=request.form['txtTitulo']
-    artista=request.form['txtArtista']
-    anio=request.form['txtAnio']
-    print(titulo,artista,anio)
 
-  return "Los datos llegaron Amigo ;)"
+    # pasamos a variables el contenido de los input
+    Vtitulo=request.form['txtTitulo']
+    Vartista=request.form['txtArtista']
+    Vanio=request.form['txtAnio']
+   
+    #Conectar y ejecutar el insert
+    cs= mysql.connection.cursor()
+    cs.execute('insert into tbAlbums (titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+    mysql.connection.commit()
+
+  flash('El album fue agregado correctamente')
+  return redirect(url_for('index'))
 
 @app.route('/eliminar')
 def eliminar():
