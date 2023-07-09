9c34f599df0aa7ec44c4ab5baefacce9d5992dfb
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2023-07-08 23:07:14 -0600

    PractFruteria Vista de consultas
    Se realizo las rutas correspondientes para una consulta

--- PractFruteria/app.py
+++ PractFruteria/app.py
@@ -49,7 +49,72 @@ def guardar():
 
 
 
+@app.route('/editar/<id>')
+def editar(id):
+  
+  CID=mysql.connection.cursor();
+  CID.execute('Select * from tbFrutas where id=%s', (id))
+  consultaID= CID.fetchone()
+  return render_template('editarFruta.html',frut=consultaID)
 
+
+
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+
+  if request.method == 'POST':
+    Vfruta=request.form['Fruta']
+    Vtemporada=request.form['Temporada']
+    Vprecio=request.form['Precio']
+    Vstock=request.form['Stock']
+
+    curAct=mysql.connection.cursor()
+    curAct.execute('update tbFrutas set fruta=%s, temporada=%s, precio=%s, stock=%s where id=%s',(Vfruta, Vtemporada, Vprecio, Vstock, id))
+    mysql.connection.commit()
+  
+  flash('Se actualizo la fruta'+ Vfruta)
+  return redirect(url_for('registros'))
+
+
+
+@app.route('/eliminar/<id>')
+def eliminar(id):
+
+  CID=mysql.connection.cursor();
+  CID.execute('Select * from tbFrutas where id=%s', (id))
+  eliminarID= CID.fetchone()
+  return render_template('eliminarFruta.html',frut=eliminarID)
+
+
+
+@app.route('/borrar/<id>', methods=['POST'])
+def borrar(id):
+
+  if request.method == 'POST':
+    
+    curElim=mysql.connection.cursor()
+    curElim.execute('delete from tbFrutas where id=%s',(id))
+    mysql.connection.commit()
+  
+  flash('Se elimino la fruta')
+  return redirect(url_for('registros'))
+
+
+
+@app.route("/consultasFrutas.html")
+def consultasFrutas():
+    return render_template('consultasFrutas.html')
+
+
+
+@app.route("/consulta")
+def consulta():
+    vfrutas = request.form.get('Fruta', False)
+    cs = mysql.connection.cursor()
+    cs.execute('select * from tbFrutas where fruta = %s order by fruta', [vfrutas])
+    data = cs.fetchone()
+    print(data)
+    return render_template('consultasFrutas.html', frut = data)
 
 
 
